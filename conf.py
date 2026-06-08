f5afd72faff8a55af0864f31e516ba3739ab0dab
CNYTUC <cuneytuc@gmail.com>
2026-06-08 17:13:26 +0300

    111

---
+++ conf.py
@@ -0,0 +1,36 @@
+# Tüm kullanıcı verilerini alır
+# ------------------------------------------------------------------
+tum_kullanicilar = None
+
+# Tüm hisse senetlerinin listesini alır
+# ------------------------------------------------------------------
+tum_hisseler = None
+
+# Verisi çekilebilen hisselerin fiyat tabloları
+# ------------------------------------------------------------------
+hisse_verileri = None
+
+# Verisi çekilebilen hisse kodları
+# ------------------------------------------------------------------
+basarili_hisseler = None
+
+# Verisi çekilemeyen hisse kodları
+# ------------------------------------------------------------------
+basarisiz_hisseler = None
+
+# SADECE RAPORLAMA İÇİN KULLANILIYOR
+# ------------------------------------------------------------------
+# Her hissenin veri çekme durumunu tutar
+# Örnek:
+# [
+#   {"Kod": "THYAO", "Durum": "✅"},
+#   {"Kod": "ADLVY", "Durum": "❌"}
+# ]
+
+veri_durumlari = None
+
+# ANALİZ İÇİN KULLANILAN DEĞİŞKENLER
+# ------------------------------------------------------------------
+analiz_sonuclari = None
+elenenler = None
+analiz_hatalari = None
